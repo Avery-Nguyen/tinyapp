a3f434a6f946c4198dc2bf8d3ee31c1fbd648841
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-08 21:01:49 +0000

    added req params and comments to functions

--- express_server.js
+++ express_server.js
@@ -9,23 +9,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //send client a reponse once they make a request
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { //lets us know that the server is on
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { //send json of the url database
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => { //hello page
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => { //shows table of the url database
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => { //user request :shortURL and server returns details page of url
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
