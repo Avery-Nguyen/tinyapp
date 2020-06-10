427e8895963385028e82f49e1b67ef051af9626a
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-10 21:59:41 +0000

    moved helper functions to helper.js

--- express_server.js
+++ express_server.js
@@ -18,6 +18,8 @@ app.use(cookieSession({
 
 const bcrypt = require('bcrypt'); //encrypt password 
 
+const { generateRandomString, emailMatch, passwordMatch, idFinder } = require('./helpers'); //helper functions
+
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: 'userRandomID'},
   "9sm5xK": {longURL: "http://www.google.com", userID: 'AVERY' }
@@ -43,34 +45,6 @@ const findData = function (obj, id) {
   return userURL;
 };
 
-const generateRandomString = function() { //creates random 6 random alphanumeric characters
-  return Math.random().toString(36).slice(2,8);
-};
-
-const emailMatch = function(obj, email) { //return true if email matches user database
-  for (const user in obj) {
-    if (obj[user].email === email) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const passwordMatch = function(obj, email) { //returns true if password matches user database
-  for (const user in obj) {
-    if (obj[user].email === email) {
-      return obj[user].password;
-    }
-  }
-};
-
-const idFinder = function(obj, email) {
-  for (const user in obj) {
-    if (obj[user].email === email) {
-      return obj[user].id;
-    }
-  }
-};
 
 app.listen(PORT, () => { //lets us know that the server is on
   console.log(`Example app listening on port ${PORT}!`);
---
+++ helpers.js
@@ -0,0 +1,30 @@
+const generateRandomString = function() { //creates random 6 random alphanumeric characters
+  return Math.random().toString(36).slice(2,8);
+};
+
+const emailMatch = function(obj, email) { //return true if email matches user database
+  for (const user in obj) {
+    if (obj[user].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const passwordMatch = function(obj, email) { //returns true if password matches user database
+  for (const user in obj) {
+    if (obj[user].email === email) {
+      return obj[user].password;
+    }
+  }
+};
+
+const idFinder = function(obj, email) {
+  for (const user in obj) {
+    if (obj[user].email === email) {
+      return obj[user].id;
+    }
+  }
+};
+
+module.exports = { generateRandomString, emailMatch, passwordMatch, idFinder }
